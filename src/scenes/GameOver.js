853bca5f058b4f37b8a28007055973c4a903c632
Everardo Robles Tena <everardoroblestena@Lalos-Air.lan>
2024-04-23 13:48:49 -0700

    added temp background

--- src/scenes/GameOver.js
+++ src/scenes/GameOver.js
@@ -9,12 +9,12 @@ export class GameOver extends Scene
 
     create ()
     {
-        this.cameras.main.setBackgroundColor(0xff0000);
+        // this.cameras.main.setBackgroundColor(0xff0000);
 
-        this.add.image(512, 384, 'background').setAlpha(0.5);
+        this.add.image(512, 384, 'gameover_background')
 
         this.add.text(512, 384, 'Game Over', {
-            fontFamily: 'Arial Black', fontSize: 64, color: '#ffffff',
+            fontFamily: 'Sans', fontSize: 64, color: '#ffffff',
             stroke: '#000000', strokeThickness: 8,
             align: 'center'
         }).setOrigin(0.5);
